9697137e813ed6c973316a441426596de22ddd13
wuxiaomiao <369987536@qq.com>
2019-10-25 17:45:14 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.less'
 
 function App() {
-  return <h1 className="logo-name">Moby</h1>
+  const [isActive, setIsActive] = useState(false)
+  const clickOnMenuButton = () => {
+    setIsActive(isActive => {
+      return !isActive
+    })
+  }
+  return (
+    <div className="container">
+      <header>
+        <div
+          className={`menu-button ${isActive ? 'is-active' : ''}`}
+          onClick={clickOnMenuButton}
+        ></div>
+        <div className="sub-menu">
+          <div className="moby">MOBY</div>
+        </div>
+      </header>
+
+      <div className={`menu-content ${isActive ? 'is-active' : ''}`}>
+        <div className="menu-wrapper">
+          <div className="menu-col">
+            <span className="item-wrapper">
+              <span className="items">
+                <span className="item-title">A</span>
+                <span className="item">a</span>
+                <span className="item">a</span>
+              </span>
+            </span>
+          </div>
+          <div className="menu-col">
+            <span className="item-wrapper">
+              <span className="items">
+                <span className="item-title">A</span>
+                <span className="item">a</span>
+                <span className="item">a</span>
+              </span>
+            </span>
+          </div>
+          <div className="menu-col">
+            <span className="item-wrapper">
+              <span className="items">
+                <span className="item-title">A</span>
+                <span className="item">a</span>
+                <span className="item">a</span>
+              </span>
+            </span>
+          </div>
+          <div className="menu-col">
+            <span className="item-wrapper">
+              <span className="items">
+                <span className="item-title">A</span>
+                <span className="item">a</span>
+                <span className="item">a</span>
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
